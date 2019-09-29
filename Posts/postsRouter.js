4f79d1dd978e286f19eb31f3d5e9813a518ff309
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-28 22:51:13 -0500

    did put, delete, and get comments

--- Posts/postsRouter.js
+++ Posts/postsRouter.js
@@ -27,9 +27,102 @@ router.get('/:id', (req, res) => {
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({error: 'posts infor could not be found'})
+        res.status(500).json({error: 'posts info could not be found'})
+    })
+});
+
+
+
+
+//   /api/posts
+router.post('/', (req, res) => {
+    const {title, contents} = req.body;
+    if (!title || !contents) {
+       return res.status(400).json({error: 'Need title and contents'}) //return stops code so it doe not go on
+    } 
+    db.insert({title, contents})
+        .then(({id}) => {
+           getPost(id, res)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Error inserting data'})
+        });
+});
+
+
+function getPost(id, res) {
+    return db.findById(id)
+        .then(([post]) => {
+            console.log(post);
+            if (post) { 
+                res.status(200).json(post);
+            } else {
+                res.status(400).json({error: `The post with the ${id} is not valid`});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: "Error getting post"});
+        });
+    }
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, comments } = req.body;
+    if (!title && !comments) {
+        res.status(400).json({error: 'Requires some changes'});
+    }
+    db.update(id, {title, comments})
+    .then(updated => {
+        if (updated){
+           getPost(id, res);
+        } else {
+            res.status(404).json({error: `post with ${id} does not exist`})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'error updating post'})
     })
 })
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+    .then((deleted) => {
+        if (deleted) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({error: 'user with id doe not exist'})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'error deleting'})
+    });
+});
+
+router.get('/:id/comments', (req, res) => {
+    const { id: post_id } = req.params;
+    db.findById(post_id)
+    .then(([post]) => {
+        if (post) {
+            db.findPostComments(post_id)
+            .then(comments => {
+                if (comments)
+                res.status(200).json({comments});
+            });
+        } else {
+            res.status(404).json({error: 'post with id does not exist'})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'error getting post comments'})
+    });
+});
+
+
 
 
 
